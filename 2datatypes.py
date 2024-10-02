1b2387b011cbb80c244856e8ed938034bca77b99
Sirpurkar-Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 18:31:21 +0530

    Add 2datatypes.py

---
+++ 2datatypes.py
@@ -0,0 +1,26 @@
+#list
+s=[10,20,30,40,50]
+print(s)
+
+#tuple
+s=(10,20,30,40,50)
+print(s)
+
+#sets
+s={ 10,20,30,40,50 }
+print(s)
+
+#dictionaries
+d1={'Name:Johan','Age:3'}
+print(d1)
+
+#string concatination
+x=int (2222.23)
+print(x)
+
+y=str(2222.23)
+print(y)
+
+z=float(2222)
+print(z)
+
