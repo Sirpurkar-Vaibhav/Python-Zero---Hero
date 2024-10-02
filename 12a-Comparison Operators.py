ffeff1904e13dbd145398eda2d483d05bb4dadc4
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 22:00:59 +0530

    Create 12a-Comparison Operators.py

---
+++ 12a-Comparison Operators.py
@@ -0,0 +1,10 @@
+# Comparison Operators
+a = 10
+b = 20
+
+print("Equal:", a == b)          # False
+print("Not Equal:", a != b)      # True
+print("Greater Than:", a > b)    # False
+print("Less Than:", a < b)       # True
+print("Greater or Equal:", a >= b)  # False
+print("Less or Equal:", a <= b)     # True
