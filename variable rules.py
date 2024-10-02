eff4dce2fd8d887e50c35065a0058d24051443a9
Sirpurkar-Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 18:25:18 +0530

    Add variable rules.py

---
+++ variable rules.py
@@ -0,0 +1,19 @@
+#under score & letter in 1st starting letter
+_var=10
+print(_var)
+
+#can't start with number
+var_iable=10
+print(var_iable)
+
+#can only contains letters & number
+var1=10
+print(var1)
+
+#Case sensitive
+a=10
+A=20
+print(a)
+print(A)
+
+#keywords can't be used as var
