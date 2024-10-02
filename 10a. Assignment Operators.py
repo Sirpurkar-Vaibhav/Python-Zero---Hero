b8ffad3734f2923a79dfb7e48fd3afbf15897c9c
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 21:55:28 +0530

    Create 10a. Assignment Operators.py

---
+++ 10a. Assignment Operators.py
@@ -0,0 +1,34 @@
+# Assignment Operators
+a = 5
+a += 3  # a = a + 3
+print("Addition Assignment:", a)  # 8
+
+a -= 2  # a = a - 2
+print("Subtraction Assignment:", a)  # 6
+
+a *= 2  # a = a * 2
+print("Multiplication Assignment:", a)  # 12
+
+a /= 3  # a = a / 3
+print("Division Assignment:", a)  # 4.0
+
+a %= 3  # a = a % 3
+print("Modulus Assignment:", a)  # 1.0
+
+a **= 2  # a = a ** 2
+print("Exponentiation Assignment:", a)  # 1.0
+
+a &= 2  # a = a & 2
+print("Bitwise AND Assignment:", a)  # 0
+
+a |= 2  # a = a | 2
+print("Bitwise OR Assignment:", a)  # 2
+
+a ^= 2  # a = a ^ 2
+print("Bitwise XOR Assignment:", a)  # 0
+
+a >>= 1  # a = a >> 1
+print("Bitwise Right Shift Assignment:", a)  # 0
+
+a <<= 1  # a = a << 1
+print("Bitwise Left Shift Assignment:", a)  # 0
