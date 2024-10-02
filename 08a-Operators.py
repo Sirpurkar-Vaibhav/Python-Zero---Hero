150efc393aa4856bcba78de1f3860ed9704b1afa
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 21:50:55 +0530

    Create 08a-Operators.py

---
+++ 08a-Operators.py
@@ -0,0 +1,21 @@
+'''I Operater
+1. Arithmetic Operators: Used for basic mathematical operations.
+   - Examples: `+`, `-`, `*`, `/`, `%`
+
+2. Assignment Operators: Used to assign values to variables.
+   - Examples: `=`, `+=`, `-=`, `*=`, `/=`, `%=`, `**=`, `&=`, `|=`, `^=`, `>>=`, `<<=`
+
+3. Logical Operators: Used to combine conditional statements.
+   - Examples: `and`, `or`, `not`
+
+4. Comparison Operators: Used to compare two values.
+   - Examples: `==`, `!=`, `<`, `>`, `<=`, `>=`
+
+5. Bitwise Operators: Perform bit-level operations.
+   - Examples: `&`, `|`, `^`, `~`, `<<`, `>>`
+
+6. Identity Operators: Used to compare objects.
+   - Examples: `is`, `is not`
+
+7. Membership Operators: Used to test if a sequence contains a value.
+   - Examples: `in`, `not in` '''
