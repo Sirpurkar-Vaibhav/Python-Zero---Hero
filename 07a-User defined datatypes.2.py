033c8aac3260c9f774f8041cffd195e53508d1af
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 21:32:47 +0530

    Create 07a-User defined datatypes.2.py

---
+++ 07a-User defined datatypes.2.py
@@ -0,0 +1,10 @@
+#aasigment multiple values
+x="Hello"
+y="I am"
+z="Vaibhav"
+print(x,y,z, sep='\n')
+#or 
+#One value to multiple variables
+x,y,z="Hello","I am","Vaibhav"
+print(x,y,z, sep='\n')
+print(x,y,z, sep='---')
