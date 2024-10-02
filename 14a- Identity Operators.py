f40f80dc64770de591e345d267d502abc443ef6d
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 22:08:07 +0530

    Create 14a- Identity Operators.py

---
+++ 14a- Identity Operators.py
@@ -0,0 +1,8 @@
+# Identity Operators
+a = [1, 2, 3] # a=['apple','banana']
+b = a
+c = [1, 2, 3] #c=['apple','banana']
+
+print("a is b:", a is b)       # True
+print("a is c:", a is c)       # False
+print("a is not c:", a is not c)  # True
