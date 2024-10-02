d3076d657a329db5348abbd9c53a1e201333a64a
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-02 22:36:30 +0530

    Create 17a- Ternary Conditional Expression.py

---
+++ 17a- Ternary Conditional Expression.py
@@ -0,0 +1,8 @@
+#value_if_true if condition else value_if_false
+a = 10
+b = 20
+
+# Using ternary operator to find the minimum value
+min_value = a if a < b else b
+print("The minimum value is:", min_value)  
+# Output: The minimum value is: 10
