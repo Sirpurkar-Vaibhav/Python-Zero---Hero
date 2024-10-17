32ffea00d571d0d203dc68933f822b63b2991229
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-18 00:31:48 +0530

    Create 27a-Nested loops.py

---
+++ 27a-Nested loops.py
@@ -0,0 +1,16 @@
+colors=["red","white","blue"]
+transport=['car','bus','bike']
+for a in colors:
+    for b in transport:
+        print(a,b)
+'''
+red car
+red bus
+red bike
+white car
+white bus
+white bike
+blue car
+blue bus
+blue bike
+'''
