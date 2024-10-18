99557a9c39d090dbb715a7846be80a85a4119364
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-19 00:22:22 +0530

    Create 30a-Return_fn.py

---
+++ 30a-Return_fn.py
@@ -0,0 +1,11 @@
+def greeting():
+    print('hello')
+    
+print(type('hello')) #not come when(type('hello') and len('hello') in fn 
+    #or 
+x=type('hello')
+print(x)
+greeting()
+''' <class 'str'>
+<class 'str'>
+hello '''
