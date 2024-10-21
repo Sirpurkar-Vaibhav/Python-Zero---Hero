a6b450c8cbf765c0320f758c5d51de622cdb73de
Sirpurkar Vaibhav <vaibhavpro98@gmail.com>
2024-10-21 22:40:13 +0530

    Update 34a-Arg2.py

--- 34a-Arg2.py
+++ 34a-Arg2.py
@@ -1,1 +1,8 @@
 about arguments
+def greet(name= 'vaibhav', message='how are you?):
+          print(f"hello {name}")
+          print(message)
+greet()
+greet('jack','what is going on')
+greet(message ='Howdy',name ='Akb')
+greet('Vaibhav', message='Good to see you!')         
